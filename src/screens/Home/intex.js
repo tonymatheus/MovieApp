f2e470d5568d5908085bf0fb836b14b981dc1331
tonymatheus <tony.chaves14@gmail.com>
2022-10-16 17:41:48 -0300

    Implements structure to  call the api movies with promisss

--- src/screens/Home/intex.js
+++ src/screens/Home/intex.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import {
   Container,
@@ -15,7 +15,64 @@ import { Header } from "../../components/Header";
 import { Feather } from "@expo/vector-icons";
 import { ScrollView } from "react-native-gesture-handler";
 import { SliderItem } from "../../components/SliderItem";
+import API, { Key } from "../../service/api";
+import axios from "axios";
+import api from "../../service/api";
+import { getListMovies } from "../../utils/movieAlgorithm";
+const { API_Key } = process.env;
+
 export const Home = () => {
+  const [nowMovies, setNowMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [topMovies, setTopMovies] = useState();
+
+  useEffect(() => {
+    const isActive = false;
+    const getMovies = async () => {
+      // const response = await API.get(`movie/now_playing/`, {
+      //   params: {
+      //     api_key: Key,
+      //     language: "pt-BR",
+      //     page: 1,
+      //   },
+      // });
+
+      //setNowMovies(response.data);
+      const [nowData, popularData, topData] = await Promise.all([
+        API.get("movie/now_playing/", {
+          params: {
+            api_key: Key,
+            language: "pt-BR",
+            page: 1,
+          },
+        }),
+        API.get("movie/popular/", {
+          params: {
+            api_key: Key,
+            language: "pt-BR",
+            page: 1,
+          },
+        }),
+        API.get("movie/top_rated/", {
+          params: {
+            api_key: Key,
+            language: "pt-BR",
+            page: 1,
+          },
+        }),
+      ]);
+      const nowList = getListMovies(10, nowData.data.results);
+      const popularList = getListMovies(4, popularData.data.results);
+      const topList = getListMovies(6, topData.data.results);
+
+      setNowMovies(nowList);
+      setPopularMovies(popularList);
+      setTopMovies(topList);
+    };
+
+    getMovies();
+  }, []);
+
   return (
     <Container>
       <Header title="PrimeFaker" />
@@ -38,27 +95,28 @@ export const Home = () => {
         <SliderMovie
           showsHorizontalScrollIndicator={false}
           horizontal={true}
-          data={[1, 2, 3, 4]}
-          key={1}
-          renderItem={({ item }) => <SliderItem />}
+          data={nowMovies}
+          renderItem={({ item }) => <SliderItem data={item} />}
+          keyExtractor={(item) => String(item.id)}
         />
         <Title>Populares</Title>
 
         <SliderMovie
           showsHorizontalScrollIndicator={false}
           horizontal={true}
-          data={[1, 2, 3, 4]}
-          key={2}
-          renderItem={({ item }) => <SliderItem />}
+          data={popularMovies}
+          key={popularMovies.id}
+          keyExtractor={(item) => String(item.id)}
+          renderItem={({ item }) => <SliderItem data={item} />}
         />
         <Title>Mais Votados</Title>
 
         <SliderMovie
           showsHorizontalScrollIndicator={false}
           horizontal={true}
-          data={[1, 2, 3, 4]}
-          key={3}
-          renderItem={({ item }) => <SliderItem />}
+          data={topMovies}
+          renderItem={({ item }) => <SliderItem data={item} />}
+          keyExtractor={(item) => String(item.id)}
         />
       </ScrollView>
     </Container>
