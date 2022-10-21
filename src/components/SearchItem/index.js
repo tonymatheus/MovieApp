7e6b50a8909339bad19a90e3809339899fe0e61c
tonymatheus <tony.chaves14@gmail.com>
2022-10-21 01:19:56 -0300

    Add search component search logic and  image background

---
+++ src/components/SearchItem/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { Container, Banner, Title, RateContainer, Rate } from "./styles";
+import { Ionicons } from "@expo/vector-icons";
+
+export const SearchItem = ({ data, navigatePage }) => {
+  const datailMovie = () => {
+    if (data.release_data === "") {
+      alert("Filme ainda não possui data");
+      return;
+    }
+    navigatePage(data);
+  };
+
+  return (
+    <Container activeOpacity={0.7} onPress={datailMovie}>
+      {data?.poster_path ? (
+        <Banner
+          source={{
+            uri: `https://image.tmdb.org/t/p/original/${data?.poster_path}`,
+          }}
+        />
+      ) : (
+        <Banner source={require("../../assets/semfoto.png")} />
+      )}
+      <Title>{data?.title}</Title>
+      <RateContainer>
+        <Ionicons name="md-star" size={12} color="#e7a74e" />
+        <Rate>{data?.vote_average}/10 </Rate>
+      </RateContainer>
+    </Container>
+  );
+};
---
+++ src/components/SearchItem/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components/native";
+
+export const Container = styled.TouchableOpacity`
+  padding: 14px;
+`;
+export const Banner = styled.Image`
+  width: 100%;
+  height: 140px;
+  border-radius: 8px;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 26px;
+  padding-top: 8px;
+`;
+export const RateContainer = styled.View`
+  flex-direction: row;
+  margin: 4px;
+  align-items: center;
+  padding-top: 4px;
+`;
+export const Rate = styled.Text`
+  color: #fff;
+  font-size: 14px;
+  margin-left: 6px;
+  font-weight: bold;
+`;
