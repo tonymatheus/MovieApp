6a1ad36deb41f2f9c04e2b40566a46460f88bbbe
tonymatheus <tony.chaves14@gmail.com>
2022-10-22 23:57:57 -0300

    Create my movie page to save move

--- src/screens/Movies/index.js
+++ src/screens/Movies/index.js
@@ -1,12 +1,38 @@
 import React from "react";
 import { View, Text } from "react-native";
 import { Header } from "../../components/Header";
+import {
+  Container,
+  MyMoviesContaier,
+  Title,
+  Rate,
+  DetailButton,
+  DetailButtonContaier,
+  MeanTitle,
+  TrashButton,
+  RateContainer,
+} from "./styles";
+import { Ionicons } from "@expo/vector-icons";
 
 export const Movies = () => {
   return (
-    <View>
+    <Container>
       <Header title="Meus filmes" />
-      <Text>Movies</Text>
-    </View>
+      <MyMoviesContaier>
+        <RateContainer>
+          <MeanTitle>Cruela</MeanTitle>
+          <Ionicons name="md-star" size={20} color="#e7a74e" />
+          <Rate>10/10</Rate>
+        </RateContainer>
+        <DetailButtonContaier>
+          <DetailButton>
+            <Title>Ver detalhes</Title>
+          </DetailButton>
+          <TrashButton>
+            <Ionicons name="md-trash" size={30} color="#FFF" />
+          </TrashButton>
+        </DetailButtonContaier>
+      </MyMoviesContaier>
+    </Container>
   );
 };
