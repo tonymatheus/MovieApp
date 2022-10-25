7affe9b443c7e8399a158a2326703c21fb105bd2
tonymatheus <tony.chaves14@gmail.com>
2022-10-25 00:48:38 -0300

    Update Babel

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,21 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin", "inline-dotenv"],
+    plugins: [
+      [
+        "module:react-native-dotenv",
+        {
+          moduleName: "@env",
+          path: ".env",
+          blacklist: null,
+          whitelist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
+      ["react-native-reanimated/plugin"],
+    ],
   };
 };
+
+//"react-native-reanimated/plugin"
